c02bec20fe4baf722b3fed9c0f77175f4365d02f
Lance <53590758+LanceWhitehorn@users.noreply.github.com>
2022-03-23 17:16:58 +1100

    added comments for typewriter and weather

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import Weather from './components/weather/weather'
 function App() {
   const name = 'Lance' // Change to your name
 
+  // Type writer effect:
+    // <Typewriter custom={true} text="What are you looking for today?" /> will use your custom text
+    // <Typewriter custom={false} /> will use randomised quotes
+  // Weather:
+    // <Weather city='{your city}' />
   return (
     <div className="container">
       <div className="content">
         <h1><Clock /></h1>
         <h1>Hello, <b>{name}.</b></h1>
-        <Typewriter text="What are you looking for today?" custom={false} />
+        <Typewriter custom={false} />
         <SearchBar />
         <Weather city="Sydney" />
       </div>
